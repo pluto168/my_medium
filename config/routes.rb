6020288a74c1c2264051aecb9cfb77cd6788f18e
pluto501 <113431769+pluto501@users.noreply.github.com>
2022-12-16 20:51:54 +0000

    1.12 - 新增使用者帳號及自我介紹

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users,controllers: {
+    registrations: 'users/registrations'
+  }
+  # devise_for :users, controllers: {
+  #       sessions: 'users/sessions'
+  #     }
 
   resources :stories
 
