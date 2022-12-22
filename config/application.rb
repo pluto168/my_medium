71deaa8f8bbe6340b5203a625615578726419a7f
pluto501 <113431769+pluto501@users.noreply.github.com>
2022-12-22 19:39:03 +0000

    1.22 B 留言功能 - 新增 Comment Model

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,13 @@ module MyMedium
     config.load_defaults 5.2
     config.time_zone = 'Asia/Taipei'
 
+    #寫入不要產生的資料夾
+    # config.generators do |g|
+    #   g.assets false 
+    #   g.helper false
+    #   g.test_framework false
+    # end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
